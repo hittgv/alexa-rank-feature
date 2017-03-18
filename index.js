b7f83b022f63ed9500516f9f34fd6dd8ec27e1aa
Manthan Dave <manthanhd@live.com>
2017-03-18 18:27:45 +0000

    Initial source commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+global.port = process.env.PORT || 3000;
+global.logger = require("winston");
+logger.level = process.env.LOG_LEVEL || "debug";
+
+//const alexa = require("alexarank");
+//app.post("/", function(req, res) {
+//    const url = req.url;
+//    
+//    alexa(url, function(err, result) {
+//        if(err) {
+//            logger.error(err);
+//            return res.status(500).send();
+//        }
+//
+//        return res.send(result);
+//    });
+//});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const morgan = require("morgan");
+app.use(morgan('combined'));
+
+const alexaData = require("alexa-traffic-rank");
+app.post("/", function(req, res) {
+    const url = req.body.url;
+    alexaData.AlexaWebData(url, function(err, result) {
+        if(err) {
+            logger.error(err);
+            return res.status(500).send();
+        }
+
+        return res.send(result);
+    });
+});
+
+app.listen(port, function(err) {
+    if(err) {
+        logger.error(`Failed to listen on port ${port}.`);
+        return process.exit(1);
+    }
+
+    logger.info(`Listening on port ${port}.`);
+});
